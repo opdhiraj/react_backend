534b0df924539af7f08b05e99bee0ca8032be5a4
opdhiraj <o.p.dhiraj@gmail.com>
2023-06-22 18:18:29 +0530

    mongodb

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, resp) => {
-  resp.send("app is working");
+require("./db/config");
+const user = require("./db/user");
+
+app.use(express.json);
+
+// app.get("/", (req, resp) => {
+//   resp.send("app is working");
+// });
+
+app.post("/register", (req, resp) => {
+  resp.send(req.body);
 });
 
 app.listen(5000);
