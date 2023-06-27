7e5a93c2e5035003591f2592bee31b59d0d706e5
Om Prakash Dhiraj <om.dhiraj@ad.infosys.com>
2023-06-27 16:17:20 +0530

    different api integration

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 require("./db/config");
 
 const User = require("./db/User");
+const Product = require("./db/Product");
+
 app.use(express.json());
 app.use(cors());
 
@@ -36,6 +38,58 @@ app.post("/login", async (req, resp) => {
   } else {
     resp.send({ result: "No user found!!!!!!!!!" });
   }
+});
+
+//for adding product
+
+app.post("/add-product", async (req, resp) => {
+  let product = new Product(req.body);
+  let result = await product.save();
+  resp.send(result);
+});
+
+//to get all products from db
+
+app.get("/products", async (req, resp) => {
+  let products = await Product.find();
+  if (products.length > 0) {
+    resp.send(products);
+  } else {
+    resp.send({ result: "No Product Found" });
+  }
+});
+
+app.delete("/products/:id", async (req, resp) => {
+  const result = await Product.deleteOne({ _id: req.params.id });
+  resp.send(result);
+});
+
+app.get("/product/:id", async (req, resp) => {
+  let result = await Product.findOne({ _id: req.params.id });
+  if (result) {
+    resp.send(result);
+  } else {
+    resp.send({ result: "No records found for /product/:id" });
+  }
+});
+
+app.put("/product/:id", async (req, resp) => {
+  let result = await Product.updateOne(
+    { _id: req.params.id },
+    { $set: req.body }
+  );
+  resp.send(result);
+});
+
+app.get("/search/:key", async (req, resp) => {
+  let result = await Product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { company: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+    ],
+  });
+  resp.send(result);
 });
 
 app.listen(5000);
